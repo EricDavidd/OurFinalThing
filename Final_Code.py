873571a022126077bd9221227dfbb9382d2fedda
RecklessCord398 <58490703+RecklessCord398@users.noreply.github.com>
2019-12-04 14:48:27 -0700

    Update Final_Code.py

--- Final_Code.py
+++ Final_Code.py
@@ -7,7 +7,7 @@ import numpy as np      # “as” lets us use standard abbreviations
 # Get the directory of this python script
 directory = os.path.dirname(os.path.abspath(__file__)) 
 # Build an absolute filename from directory + filename
-filename = os.path.join(directory, 'ISTE.png')
+filename = os.path.join(directory, 'ISTE.png') #Change the code 'ISTE.png' to the name of your image file
 # Read the image data into an array
 img = plt.imread(filename)
 
